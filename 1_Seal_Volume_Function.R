a3438725f14c18033af38d13aa3a00c66162e731
Daire Carroll <136069419+DaireCarroll2023@users.noreply.github.com>
2023-06-28 12:33:02 +0200

    Update 1_Seal_Volume_Function.R

--- 1_Seal_Volume_Function.R
+++ 1_Seal_Volume_Function.R
@@ -1,3 +1,4 @@
+#Daire Carroll, Gothenburg University, 2023, carrolldaire@gmail.com
 #3: Approaching a population level assessment of body size in pinnipeds using drones, an early warning of environmental degradation.
 #Automatically estimate length, width, elipsoid volume and complex volume for spatial polygons representing the outlines of harbour seals.
 
